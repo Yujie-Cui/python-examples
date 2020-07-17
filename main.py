0a658e8df6395081e5be4d7a8837e137404afc26
cyj <YujieCui@pku.edu.cn>
2020-07-17 22:42:06 +0800

    add Del and query

--- main.py
+++ main.py
@@ -2,8 +2,13 @@ import func
 dict={}
 func.showList()
 sel = int(input())
-if sel==1:
-	dict = func.addEmployee(dict);
-	print(dict[1]['name'])
-#elif:
-	#func.delEmployee()
+while sel!=0:
+	if sel==1:
+		dict = func.addEmployee(dict);
+		print(dict[1]['name'])
+	elif sel==2:
+		dict = func.delEmployee(dict)
+	elif sel ==3 :
+		dict = func.queryEmployee(dict)
+	func.showList()
+	sel = int(input())
