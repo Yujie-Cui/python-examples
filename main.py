abfef109dcd66fce64e716c065f71f051be1aca7
cyj <YujieCui@pku.edu.cn>
2020-07-20 20:57:35 +0800

    supplement main.py

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import func
 dict={}
 func.showList()
+func.readInfo(dict)
 sel = int(input())
-while sel!=0:
+while True:
 	if sel==1:
 		dict = func.addEmployee(dict);
-		print(dict[1]['name'])
 	elif sel==2:
 		dict = func.delEmployee(dict)
-	elif sel ==3 :
+	elif sel ==3:
+		dict =func.alterInfo(dict)
+	elif sel ==4 :
 		dict = func.queryEmployee(dict)
+	elif sel==5:
+		func.displayEmployee(dict)
+	else :
+		func.exitSystem(dict)
+		break
 	func.showList()
 	sel = int(input())
